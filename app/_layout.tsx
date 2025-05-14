7ae04e0b6319315c07d6edf89b3502cbca8594f6
Gai Isay <gaiisay04@gmail.com>
2025-05-14 09:24:17 +0200

    feat: add simple first screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,5 @@
 import { Stack } from "expo-router";
-import { createTamagui, TamaguiProvider, YStack } from "tamagui";
+import { createTamagui, TamaguiProvider } from "tamagui";
 import { defaultConfig } from "@tamagui/config/v4";
 import { StatusBar } from "expo-status-bar";
 const config = createTamagui(defaultConfig);
@@ -7,23 +7,11 @@ const config = createTamagui(defaultConfig);
 export default function RootLayout() {
   return (
     <TamaguiProvider config={config}>
-      <StatusBar />
+      <StatusBar style="dark" />
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
-      {/* <YStack
-        flex={1}
-        borderWidth={2}
-        borderColor="$color"
-        borderRadius="$4"
-        gap="$2"
-        padding="$2"
-      >
-        <YStack backgroundColor="$color" borderRadius="$3" padding="$2" />
-        <YStack backgroundColor="$color" borderRadius="$3" padding="$2" />
-        <YStack backgroundColor="$color" borderRadius="$3" padding="$2" />
-      </YStack> */}
     </TamaguiProvider>
   );
 }
