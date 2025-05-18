c2605c31449ea556018e353a8a437b13edfe3959
Gai Isay <gaiisay04@gmail.com>
2025-05-18 18:58:54 +0200

    setup nativewind and rnr

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,23 +1,18 @@
-import { Link } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { Link, Stack } from "expo-router";
+import { View } from "react-native";
+import { Text } from "~/components/ui/text";
 
 export default function NotFoundScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        padding: 16,
-      }}
-    >
-      <Text>404</Text>
-      <Text style={{ fontSize: 16, textAlign: "center" }}>
-        Oops! The page you're looking for doesn't exist.
-      </Text>
-      <Link href="/" asChild>
-        <Button title="Go Home" />
-      </Link>
+    <View>
+      <Stack.Screen options={{ title: "Oops!" }} />
+      <View>
+        <Text>This screen doesn&apos;t exist.</Text>
+
+        <Link href="/">
+          <Text>Go to home screen!</Text>
+        </Link>
+      </View>
     </View>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,38 @@
-import React from "react";
-import {
-  SafeAreaView,
-  Keyboard,
-  TouchableWithoutFeedback,
-  View,
-  Text,
-} from "react-native";
+import { useState } from "react";
+import { SafeAreaView, View } from "react-native";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
+import { Text } from "~/components/ui/text";
 
-export default function Index() {
+export default function Screen() {
+  const [activeTab, setActiveTab] = useState("today");
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View
-          style={{
-            flex: 1,
-            padding: 16,
-          }}
+    <SafeAreaView>
+      <View className="p-2 flex-1">
+        <Tabs
+          value={activeTab}
+          onValueChange={setActiveTab}
+          className="w-full mx-auto flex-col gap-1.5"
         >
-          <Text>Sisu</Text>
-        </View>
-      </TouchableWithoutFeedback>
+          <TabsList className="flex-row w-full gap-4">
+            <TabsTrigger value="today" className="flex-1">
+              <Text>Today</Text>
+            </TabsTrigger>
+            <TabsTrigger value="tomorrow" className="flex-1">
+              <Text>Tomorrow</Text>
+            </TabsTrigger>
+            <TabsTrigger value="habits" className="flex-1">
+              <Text>Habits</Text>
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="account">
+            <Text>test</Text>
+          </TabsContent>
+          <TabsContent value="password">
+            <Text>test2</Text>
+          </TabsContent>
+        </Tabs>
+      </View>
     </SafeAreaView>
   );
 }
